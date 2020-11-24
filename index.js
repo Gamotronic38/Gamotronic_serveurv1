7a61ba98817d8f507694f19d28d78df3c705408e
Gamotronic38 <74872770+Gamotronic38@users.noreply.github.com>
2020-11-24 20:42:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,44 +20,7 @@ bot.on('message', message => {
 
     if (message.content ===  (conf.prefix)+' help'){
         message.channel.send(help);
-        {
-    "embed": {
-      "title": "HELP",
-      "description": " prefix : ```!G```",
-      "color": 7564251,
-      "thumbnail": {
-        "url": "https://cdn.discordapp.com/icons/763447797143044126/3d302e3f66a94d02b1e14d822bb16dfd.png?size=128"
-      },
-      "image": {
-        "url": "https://th.bing.com/th/id/OIP.uw3MABmuduKXuyseK_nGlQHaHa?pid=Api&rs=1"
-      },
-      "fields": [
-                        {
-          "name": "!G help en ",
-          "value": "this page in english "
-        },
-        {
-          "name": "wech bot",
-          "value": "répond à ton message par une réaction alléatoire"
-        },
-        {
-          "name": "!G registre [évènement_name]",
-          "value": "te registre pour un évènement et a la place de [évènement_name] vous pouvez mettre [test] pour voir si le registre marche bien"
-        },
-  
-        {
-          "name": "!G INVITE",
-          "value": "lien officiel d'invite du serveur",
-          "inline": true
-        },
-        {
-          "name": "!G YT",
-          "value": "La chaine youtube du créateur",
-          "inline": true
-        }
-      ]
-    }
-}
+       }
     }
     if (message.content === (conf.prefix)+' help en'){
       message.channel.send(help_en);
